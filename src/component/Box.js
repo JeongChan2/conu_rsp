1166ce0aefc329f55c011450a4c2e1ac3b9a7b74
JeongChan2 <jcstar55@gmail.com>
2024-08-13 23:30:56 +0900

    feat: default image insert

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Box = (props) => {
-  return (
+  return ( // src={props.item && props.item.img}
     <div className='box'>
       <h1>{props.title}</h1>
-      <img className='item-img' src={props.item && props.item.img} alt='none'/>
+      <img className='item-img' src={props.item?props.item.img:"https://cdn-icons-png.flaticon.com/512/4975/4975327.png"} alt='none'/>
       <h2>WIN</h2>
     </div>
   )
