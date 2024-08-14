49a8c6b4ffc366ecf5d4913af3b3e9f01a97dac5
JeongChan2 <jcstar55@gmail.com>
2024-08-15 00:17:06 +0900

    feat: rock scissors paper

--- src/component/Popup.js
+++ src/component/Popup.js
@@ -2,14 +2,17 @@ import React from "react";
 import './css/Popup.css';
 
 const Popup = ({ onClose, result}) => {
-  const resultContents = (result === "tie"?"분발해":result==="win"?"축하해":"더 분발해")
+  // const resultContents = (result === "tie"?"분발해":result==="win"?"축하해":"더 분발해")
+  const handleClose = (e) => {
+    e.stopPropagation(); // 클릭 이벤트 전파 방지
+    onClose();
+  }; 
   return (
-      <div className="popup">
-          <div className="popup-content">
-              <span className="close" onClick={onClose}>&times;</span>
+      <div className="popup" onClick={onClose}>
+          <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+              <span className="close" onClick={handleClose}>&times;</span>
               <h2>{result}</h2>
-              <div className="tmp">{resultContents}</div>
-              <img src="./resources/rsp.gif" alt="ss"/>
+              <div className={`item-img ${result}-gif`}></div>
           </div>
       </div>
   );
